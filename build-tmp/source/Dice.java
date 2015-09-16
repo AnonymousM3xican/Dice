65337f568b399910045dcbdef1013bc3ca086cab
unknown <vimartinez4@697-334-Waymoot.sf.k12.internal>
2015-09-16 08:23:47 -0700

    The Final Dice Push

--- build-tmp/source/Dice.java
+++ build-tmp/source/Dice.java
@@ -1,9 +1,25 @@
-void setup()
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Dice extends PApplet {
+
+public void setup()
 {
 	noLoop();
 	size(500,600);
 }
-void draw()
+public void draw()
 {
 	background(250,250,250);
 	int count=0;
@@ -25,7 +41,7 @@ void draw()
 
 }
 
-void mousePressed()
+public void mousePressed()
 {
 	redraw();
 }
@@ -40,11 +56,11 @@ class Die //models one single dice cube
 		myY=y;
 		numDot=0;
 	}
-	void roll()
+	public void roll()
 	{
 		numDot=(int)(Math.random()*6)+1;
 	}
-	void show()
+	public void show()
 	{
 		stroke(0,0,0);
 		fill(250,250,250);
@@ -89,4 +105,13 @@ class Die //models one single dice cube
 		ellipse(myX+25,myY+15,5,5);
 		}
 	}
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Dice" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
